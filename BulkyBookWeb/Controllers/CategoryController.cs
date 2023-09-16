dbb01934cc6d4375b9e5308ae3d1fcd486f8e7e4
Hubert J <hubert.janiuk.2ti@gmail.com>
2023-09-17 01:00:48 +0200

    Added "Edit" feature

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,6 +36,35 @@ namespace BulkyBookWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
-        }  
-    }
+        }
+
+		//GET
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0) 
+            { 
+                return NotFound();
+            }
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+			return View(category);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Update(obj);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+	}
 }
