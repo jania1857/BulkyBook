f1130bef5cf00424e86103d9d4fc4b52a0a31f22
Hubert J <hubert.janiuk.2ti@gmail.com>
2023-09-16 22:57:47 +0200

    Added few features

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategory = _dbContext.Categories;
+            return View(objCategory);
+        }
+    }
+}
